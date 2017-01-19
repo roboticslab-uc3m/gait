069999724c1d13618b3c014043cb1622481ee4ae
munozyanez <munoz.yanez@gmail.com>
2017-01-19 17:15:54 +0100

    modified:   src/Oscillator.cpp 	modified:   src/Oscillator.hpp

--- src/Oscillator.cpp
+++ src/Oscillator.cpp
@@ -3,10 +3,32 @@
 Oscillator::Oscillator()
 {
 
+    Initialization(1,1/10);
 }
 
-Oscillator::Oscillator(double periodT, double center, double positiveAmplitude, double negativeAmplitude)
+Oscillator::Oscillator(double periodT, double positiveAmplitude, double negativeAmplitude)
 {
 
+    Initialization(periodT, periodT/10);
+    double amp;
+    //double timeRate = positiveAmplitude/(positiveAmplitude - negativeAmplitude);
+    //double pT= timeRate*T;
+    //double pSamples = (int) (1/timeRate);
+    //double aN= negativeAmplitude/(positiveAmplitude - negativeAmplitude);
+    unsigned int pSamples = (int) samples * negativeAmplitude/(positiveAmplitude - negativeAmplitude);
+    //zero position crossing at
+    for (int i=0; i<pSamples; i++)
+    {
+
+    }
+
+
+}
+
+bool Oscillator::Initialization(double newT, double newTs)
+{
+    T=newT;
+    Ts=newTs;
+    samples = T/Ts;
 
 }
--- src/Oscillator.hpp
+++ src/Oscillator.hpp
@@ -7,14 +7,17 @@ class Oscillator
 {
 public:
     Oscillator();
-    Oscillator(double periodT, double center, double positiveAmplitude, double negativeAmplitude);
+    Oscillator(double periodT, double positiveAmplitude, double negativeAmplitude);
 
 
 private:
-    double T; //period
+    double T; //period in seconds
     double Ts; //time between two positions
+    unsigned int samples; //number of samples per period
     std::vector<double> positionProfile;
     std::vector<double> velocityProfile;
+
+    bool Initialization(double newT, double newTs);
 };
 
 #endif // OSCILLATOR_HPP
